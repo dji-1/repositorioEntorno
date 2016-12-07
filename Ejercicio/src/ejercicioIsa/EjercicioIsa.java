75f790ce83db9c37765ccf56f4e1fefe409a1652
dam1-mimanchadog <Isa@Isa>
2016-12-07 20:34:59 +0100

    Se crea mi ejercicio

---
+++ Ejercicio/src/ejercicioIsa/EjercicioIsa.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicioIsa;
+
+/**
+ *
+ * @author Isa
+ */
+public class EjercicioIsa {
+    public static void main(String[] args) {
+       int num = 20;
+       
+        while (num<=160) {
+            if(num%2==0){
+            }
+            else {
+                System.out.println(num);
+            }
+            num++;
+        }
+    }
+}
